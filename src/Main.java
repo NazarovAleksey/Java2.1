c60403f7db9b03f488777814b58609b6508bb228
Alexey Nazarov <alexeyka1987@gmail.com>
2020-05-05 10:21:50 +0500

    Add Money Transfer

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int amount = 500_000_000;
+        int result = balance + amount;
+
+        System.out.println(result);
+    }
+}
